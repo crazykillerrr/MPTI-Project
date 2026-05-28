df4166499c5cc411ba86b135a005df57ec51fbca
Syauqi Rahmat <qiraa13@users.noreply.github.com>
2026-05-28 22:35:49 +0700

    ngulang push manual

--- PerabotiQ/resources/views/aksesoris.blade.php
+++ PerabotiQ/resources/views/aksesoris.blade.php
@@ -40,9 +40,9 @@
       <!-- NAV MENU -->
       <nav id="navmenu">
         <ul class="navmenu">
-          <li><a href="#hero">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#products">Products</a></li>
+          <li><a href="/#hero">Home</a></li>
+          <li><a href="/#about">About</a></li>
+          <li><a href="/#products">Products</a></li>
         </ul>
       </nav>
   
